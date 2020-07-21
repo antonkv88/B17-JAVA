e0a2bfa72b3900f688a83225ec0482839e2cf614
Antony88 <antonkv88@gmail.com>
2020-07-22 00:50:17 +0300

    Создан класс point и main
    В main реализован метод точка входа. Класс point реализует точку в 2 мерном пространстве

---
+++ sandbox/src/main/java/task02/Main.java
@@ -0,0 +1,13 @@
+package task02;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Расстояние между 2-мя точками (отдельная функция): " + distance(new Point(10,20), new Point (0,0)));
+    System.out.println("Расстояние между 2-мя точками (метод класса point): " + Point.distance(new Point(10,20), new Point (0,0)));
+  }
+
+  public static double distance(Point p1, Point p2) {
+    return Math.sqrt( Math.pow((p1.x - p2.x),2) + Math.pow((p1.y - p2.y),2));
+  }
+
+}
