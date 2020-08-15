a24208c03e22c357d9eabcf1ca4dc537d91b2064
Antony88 <antonkv88@gmail.com>
2020-08-16 00:04:25 +0300

    создание группы и нахождение max через лямбду
    практика по заданию группы

--- addressbook-web-tests/src/test/java/tests/CreatGroupTest.java
+++ addressbook-web-tests/src/test/java/tests/CreatGroupTest.java
@@ -4,6 +4,7 @@ import model.GroupData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class CreatGroupTest extends TestBase {
@@ -12,10 +13,16 @@ public class CreatGroupTest extends TestBase {
   public void testGroupCreation() throws Exception {
   app.getNavigationHelper().gotoGroupPage();
   List<GroupData> before = app.getGroupHelper().getGroupList();
-  app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
+  GroupData group = new GroupData("test2", null, null);
+  app.getGroupHelper().createGroup(group);
   app.getNavigationHelper().returnGroupPage();
   List<GroupData> after = app.getGroupHelper().getGroupList();
   Assert.assertEquals(after.size(), before.size() + 1);
+
+  group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(),o2.getId())).get().getId());
+  before.add(group);
+
+  Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
 }
 
 }
--- addressbook-web-tests/src/test/java/tests/ModifyGroupTest.java
+++ addressbook-web-tests/src/test/java/tests/ModifyGroupTest.java
@@ -4,6 +4,7 @@ import model.GroupData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class ModifyGroupTest extends TestBase{
@@ -15,13 +16,18 @@ public void testModifyGroup(){
       app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
     }
     List<GroupData> before = app.getGroupHelper().getGroupList();
-    app.getGroupHelper().selectGroup(before.size() -1);
+    app.getGroupHelper().selectGroup(before.size() - 1);
     app.getGroupHelper().initGroupModify();
-    app.getGroupHelper().fillGroupCreation(new GroupData("modify1", "modify2", "modify3"));
+    GroupData group = new GroupData(before.get(before.size() - 1).getId(),"modify1", "modify2", "modify3");
+    app.getGroupHelper().fillGroupCreation(group);
     app.getGroupHelper().submitGroupModify();
     app.getNavigationHelper().returnGroupPage();
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size());
+
+    before.remove(before.size() -1);
+    before.add(group);
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
 
 }
