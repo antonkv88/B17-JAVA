5a2d6b57a067291de4b28bfd5d7cfb884111ae71
Antony88 <antonkv88@gmail.com>
2020-08-18 22:51:10 +0300

    запуск одного экземпляра браузера

--- addressbook-web-tests/src/test/java/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/tests/TestBase.java
@@ -2,19 +2,19 @@ package tests;
 
 import appmanager.ApplicationManager;
 import org.openqa.selenium.remote.BrowserType;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 
 public class TestBase {
 
-protected ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+protected static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
-@BeforeMethod(alwaysRun = true)
+@BeforeSuite
 protected void setUp() throws Exception {
   app.init();
 }
 
-@AfterMethod(alwaysRun = true)
+@AfterSuite
 protected void tearDown() throws Exception {
   app.stop();
 }
