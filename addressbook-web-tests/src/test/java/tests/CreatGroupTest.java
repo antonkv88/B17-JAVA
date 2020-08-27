f81ea5709d0d2e24ed198f26477c3cfab0e1742a
Antony88 <antonkv88@gmail.com>
2020-08-27 22:11:20 +0300

    параметризованные тесты создания групп и контактов

--- addressbook-web-tests/src/test/java/tests/CreatGroupTest.java
+++ addressbook-web-tests/src/test/java/tests/CreatGroupTest.java
@@ -2,18 +2,31 @@ package tests;
 
 import model.GroupData;
 import model.Groups;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class CreatGroupTest extends TestBase {
 
-@Test
-  public void testGroupCreation() throws Exception {
+@DataProvider
+public Iterator<Object[]> validGroups(){
+  List<Object[]> list = new ArrayList<Object[]>();
+  list.add(new Object[] {new GroupData().withName("test1").withHeader("header1").withFooter("footer1")});
+  list.add(new Object[] {new GroupData().withName("test2").withHeader("header2").withFooter("footer2")});
+  list.add(new Object[] {new GroupData().withName("test3").withHeader("header3").withFooter("footer3")});
+  return list.iterator();
+}
+
+@Test(dataProvider = "validGroups")
+  public void testGroupCreation(GroupData group) throws Exception {
   app.goTo().groupPage();
-  Groups before = app.group().all();
-  GroupData group = new GroupData().withName("test2");
+  Groups before = app.group().all();;
   app.group().create(group);
   app.group().returnGroupPage();
   Groups after = app.group().all();
