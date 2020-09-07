57c754201f10013ab32e207647137187137dacd9
Antony88 <antonkv88@gmail.com>
2020-09-07 23:15:55 +0300

    добавлено логирование запуска и остановки тестов logback

--- addressbook-web-tests/src/test/java/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/tests/TestBase.java
@@ -2,10 +2,18 @@ package tests;
 
 import appmanager.ApplicationManager;
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
+import java.lang.reflect.Method;
+
 public class TestBase {
+
+Logger logger = LoggerFactory.getLogger(TestBase.class);
 
 protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
@@ -17,6 +25,16 @@ protected void setUp() throws Exception {
 @AfterSuite
 protected void tearDown() throws Exception {
   app.stop();
+}
+
+@BeforeMethod
+protected void logTestStart(Method m){
+  logger.info("Start test testGroupCreation" + m.getName());
+}
+
+@AfterMethod(alwaysRun = true)
+protected void logTestStop(Method m){
+  logger.info("Stop test testGroupCreation" + m.getName());
 }
 
 }
